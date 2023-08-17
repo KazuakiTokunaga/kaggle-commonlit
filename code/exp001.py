d5eaa8255fc144d430ee179675ffda65a0f2e365
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2023-08-17 22:10:43 +0900

    update

---
+++ code/exp001.py
@@ -0,0 +1,240 @@
+from typing import List
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import warnings
+import logging
+import os
+import shutil
+import json
+import transformers
+from transformers import AutoModel, AutoTokenizer, AutoConfig, AutoModelForSequenceClassification
+from transformers import DataCollatorWithPadding
+from datasets import Dataset,load_dataset, load_from_disk
+from transformers import TrainingArguments, Trainer
+from datasets import load_metric, disable_progress_bar
+from sklearn.metrics import mean_squared_error
+import torch
+from sklearn.model_selection import KFold, GroupKFold
+from tqdm import tqdm
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+from collections import Counter
+import spacy
+import re
+from autocorrect import Speller
+from spellchecker import SpellChecker
+import lightgbm as lgb
+
+
+# set random seed
+def seed_everything(seed: int):
+    
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+
+class CFG:
+    model_name="debertav3base"
+    learning_rate=1.5e-5
+    weight_decay=0.02
+    hidden_dropout_prob=0.005
+    attention_probs_dropout_prob=0.005
+    num_train_epochs=5
+    n_splits=4
+    batch_size=12
+    random_seed=42
+    save_steps=100
+    max_length=512
+
+
+class Preprocessor:
+    def __init__(
+        self, 
+        model_name: str,
+    ) -> None:
+
+        self.tokenizer = AutoTokenizer.from_pretrained(f"/kaggle/input/{model_name}")
+        self.STOP_WORDS = set(stopwords.words('english'))
+        
+        self.spacy_ner_model = spacy.load('en_core_web_sm',)
+        self.speller = SpellChecker() #Speller(lang='en')
+        
+    def count_text_length(self, df: pd.DataFrame, col:str) -> pd.Series:
+        """ text length """
+        tokenizer=self.tokenizer
+        return df[col].progress_apply(lambda x: len(tokenizer.encode(x)))
+
+    def word_overlap_count(self, row):
+        """ intersection(prompt_text, text) """        
+        def check_is_stop_word(word):
+            return word in self.STOP_WORDS
+        
+        prompt_words = row['prompt_tokens']
+        summary_words = row['summary_tokens']
+        if self.STOP_WORDS:
+            prompt_words = list(filter(check_is_stop_word, prompt_words))
+            summary_words = list(filter(check_is_stop_word, summary_words))
+        return len(set(prompt_words).intersection(set(summary_words)))
+            
+    def ngrams(self, token, n):
+        # Use the zip function to help us generate n-grams
+        # Concatentate the tokens into ngrams and return
+        ngrams = zip(*[token[i:] for i in range(n)])
+        return [" ".join(ngram) for ngram in ngrams]
+
+    def ngram_co_occurrence(self, row, n: int):
+        # Tokenize the original text and summary into words
+        original_tokens = row['prompt_tokens']
+        summary_tokens = row['summary_tokens']
+
+        # Generate n-grams for the original text and summary
+        original_ngrams = set(self.ngrams(original_tokens, n))
+        summary_ngrams = set(self.ngrams(summary_tokens, n))
+
+        # Calculate the number of common n-grams
+        common_ngrams = original_ngrams.intersection(summary_ngrams)
+
+        # # Optionally, you can get the frequency of common n-grams for a more nuanced analysis
+        # original_ngram_freq = Counter(ngrams(original_words, n))
+        # summary_ngram_freq = Counter(ngrams(summary_words, n))
+        # common_ngram_freq = {ngram: min(original_ngram_freq[ngram], summary_ngram_freq[ngram]) for ngram in common_ngrams}
+
+        return len(common_ngrams)
+    
+    def ner_overlap_count(self, row, mode:str):
+        model = self.spacy_ner_model
+        def clean_ners(ner_list):
+            return set([(ner[0].lower(), ner[1]) for ner in ner_list])
+        prompt = model(row['prompt_text'])
+        summary = model(row['text'])
+
+        if "spacy" in str(model):
+            prompt_ner = set([(token.text, token.label_) for token in prompt.ents])
+            summary_ner = set([(token.text, token.label_) for token in summary.ents])
+        elif "stanza" in str(model):
+            prompt_ner = set([(token.text, token.type) for token in prompt.ents])
+            summary_ner = set([(token.text, token.type) for token in summary.ents])
+        else:
+            raise Exception("Model not supported")
+
+        prompt_ner = clean_ners(prompt_ner)
+        summary_ner = clean_ners(summary_ner)
+
+        intersecting_ners = prompt_ner.intersection(summary_ner)
+        
+        ner_dict = dict(Counter([ner[1] for ner in intersecting_ners]))
+        
+        if mode == "train":
+            return ner_dict
+        elif mode == "test":
+            return {key: ner_dict.get(key) for key in self.ner_keys}
+
+    
+    def quotes_count(self, row):
+        summary = row['text']
+        text = row['prompt_text']
+        quotes_from_summary = re.findall(r'"([^"]*)"', summary)
+        if len(quotes_from_summary)>0:
+            return [quote in text for quote in quotes_from_summary].count(True)
+        else:
+            return 0
+
+    def spelling(self, text):
+        
+        wordlist=text.split()
+        amount_miss = len(list(self.speller.unknown(wordlist)))
+
+        return amount_miss
+    
+    def run(self, 
+            prompts: pd.DataFrame,
+            summaries:pd.DataFrame,
+            mode:str
+        ) -> pd.DataFrame:
+        
+        # before merge preprocess
+        prompts["prompt_length"] = prompts["prompt_text"].apply(
+            lambda x: len(self.tokenizer.encode(x))
+        )
+        prompts["prompt_tokens"] = prompts["prompt_text"].apply(
+            lambda x: self.tokenizer.convert_ids_to_tokens(
+                self.tokenizer.encode(x), 
+                skip_special_tokens=True
+            )
+        )
+
+        summaries["summary_length"] = summaries["text"].apply(
+            lambda x: len(self.tokenizer.encode(x))
+        )
+        summaries["summary_tokens"] = summaries["text"].apply(
+            lambda x: self.tokenizer.convert_ids_to_tokens(
+                self.tokenizer.encode(x), 
+                skip_special_tokens=True
+            )
+
+        )
+        summaries["splling_err_num"] = summaries["text"].progress_apply(self.spelling)
+
+        # merge prompts and summaries
+        input_df = summaries.merge(prompts, how="left", on="prompt_id")
+
+        # after merge preprocess
+        input_df['length_ratio'] = input_df['summary_length'] / input_df['prompt_length']
+        
+        input_df['word_overlap_count'] = input_df.progress_apply(self.word_overlap_count, axis=1)
+        input_df['bigram_overlap_count'] = input_df.progress_apply(
+            self.ngram_co_occurrence,args=(2,), axis=1 
+        )
+        input_df['trigram_overlap_count'] = input_df.progress_apply(
+            self.ngram_co_occurrence, args=(3,), axis=1
+        )
+        
+        # Crate dataframe with count of each category NERs overlap for all the summaries
+        # Because it spends too much time for this feature, I don't use this time.
+#         ners_count_df  = input_df.progress_apply(
+#             lambda row: pd.Series(self.ner_overlap_count(row, mode=mode), dtype='float64'), axis=1
+#         ).fillna(0)
+#         self.ner_keys = ners_count_df.columns
+#         ners_count_df['sum'] = ners_count_df.sum(axis=1)
+#         ners_count_df.columns = ['NER_' + col for col in ners_count_df.columns]
+#         # join ner count dataframe with train dataframe
+#         input_df = pd.concat([input_df, ners_count_df], axis=1)
+        
+        input_df['quotes_count'] = input_df.progress_apply(self.quotes_count, axis=1)
+        
+        return input_df.drop(columns=["summary_tokens", "prompt_tokens"])
+    
+
+
+def main():
+    # logging setting 
+
+    warnings.simplefilter("ignore")
+    logging.disable(logging.ERROR)
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+    disable_progress_bar()
+    tqdm.pandas()
+
+    seed_everything(seed=42)
+
+    DATA_DIR = "/kaggle/input/commonlit-evaluate-student-summaries/"
+
+    prompts_train = pd.read_csv(DATA_DIR + "prompts_train.csv")
+    prompts_test = pd.read_csv(DATA_DIR + "prompts_test.csv")
+    summaries_train = pd.read_csv(DATA_DIR + "summaries_train.csv")
+    summaries_test = pd.read_csv(DATA_DIR + "summaries_test.csv")
+    sample_submission = pd.read_csv(DATA_DIR + "sample_submission.csv")
+
+
+    #summaries_train = summaries_train.head(10) # for dev mode
+
+    preprocessor = Preprocessor(model_name=CFG.model_name)
