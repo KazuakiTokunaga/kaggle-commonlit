869b0b5caa8373db844eef4745324b52f7725b6e
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2023-08-17 22:25:37 +0900

    update

--- commonlit/exp001.py
+++ commonlit/exp001.py
@@ -31,10 +31,6 @@ import lightgbm as lgb
 
 # set random seed
 def seed_everything(seed: int):
-
-    import random, os
-    import numpy as np
-    import torch
     
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
