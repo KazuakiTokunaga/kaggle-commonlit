02798d20863fe93e750cf43b9069f374147205e4
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2023-09-12 21:37:34 +0900

    update

--- commonlit/exp003.py
+++ commonlit/exp003.py
@@ -938,7 +938,7 @@ class Runner():
     def preprocess(self):
 
         if RCFG.use_preprocessed_dataset:
-            nrows = RCFG.debug_size if RCFG.debug else None:
+            nrows = RCFG.debug_size if RCFG.debug else None
             self.train = pd.read_csv(f"{RCFG.preprocessed_dataset_path}/{RCFG.use_train_data_file}", nrows=nrows)
             self.test = pd.read_csv(f"{RCFG.preprocessed_dataset_path}/{RCFG.use_test_data_file}")
 
