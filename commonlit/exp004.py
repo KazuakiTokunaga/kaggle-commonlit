5c57158999e7b8b2ca26b79ee211e2e46c9a07d5
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2023-10-08 21:06:58 +0900

    update

--- commonlit/exp004.py
+++ commonlit/exp004.py
@@ -1374,7 +1374,7 @@ class Runner():
                     trues.extend(y_eval_cv)
                     preds_tmp.extend(pred)
                 
-                preds = np.mean(np.array([preds, preds_tmp]), axis=0)
+                preds = np.array([preds, preds_tmp]).mean(axis=0)
                 
             rmse = np.sqrt(mean_squared_error(trues, preds))
             self.logger.info(f"{target}_rmse : {rmse}")
